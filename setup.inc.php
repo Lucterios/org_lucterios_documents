28da9265639c14aa7c3cd0a6b468b5eb073b936e
user <user@69e63612-969a-4371-9866-b14aae95a0cb>
2011-11-29 02:04:17 +0000

    Modifications module org_lucterios_documents (1.3.1.163)
    git-svn-id: http://projets.lucterios.org/svn/extensions/org_lucterios_documents@1473 69e63612-969a-4371-9866-b14aae95a0cb

--- setup.inc.php
+++ setup.inc.php
@@ -18,7 +18,7 @@
 // 
 // 		Contributeurs: Fanny ALLEAUME, Pierre-Olivier VERSCHOORE, Laurent GAY
 // setup file write by SDK tool
-// --- Last modification: Date 11 November 2011 10:54:30 By  ---
+// --- Last modification: Date 29 November 2011 0:19:05 By  ---
 
 $extention_name="org_lucterios_documents";
 $extention_description="Gestion de documents et de fichiers partagés";
@@ -30,7 +30,7 @@ $extension_libre=true;
 $version_max=1;
 $version_min=3;
 $version_release=1;
-$version_build=162;
+$version_build=163;
 
 $depencies=array();
 $depencies[0] = new Param_Depencies("CORE", 1, 3, 1, 3, false);
